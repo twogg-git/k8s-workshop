2cd67caf140fdd2d3155d053b7e666cf95ef00d0
Catherin Cruz <cruz.catherin@gmail.com>
2018-09-30 13:29:37 -0500

    Modifying html output

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -20,7 +20,7 @@ func getServerIP() string {
 }
 
 func getHTML(image string) string {
-	message := "Customer Support, Mr. Cat speaking, how can I help you?"
+	message := "TechSupport, Mr. Cat speaking, how can I help you?"
 	color := "orange"
 	if image == "1.3.1" {
 		message = "High five budy, I'm healthy as ever!"
@@ -31,7 +31,7 @@ func getHTML(image string) string {
 		color = "red"
 	}
 	return `<!DOCTYPE html><html><body><center>
-			<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src` + image + `.png">
+			<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/` + image + `.png">
 			<h1 style="color:` + color + `">` + message + `</h3>	
 			<h2 style="color:green">Playing with Kubernetes</h1>
 			<h2 style="color:blue">Server IP ` + getServerIP() + `</h2>
