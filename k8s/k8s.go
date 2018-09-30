a7189f3ee3c6b5f0d4766cb18fc8670c5b120b3e
Catherin Cruz <cruz.catherin@gmail.com>
2018-09-30 01:42:11 -0500

    Adding server ip recognition, new header image

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
+func getServerIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP.String()
+}
+
 func playHome(w http.ResponseWriter, r *http.Request) {
 	html := `<!DOCTYPE html><html><body><center>
-		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/1.0.png">
-		<h1 style="color:green">Playing with Kubernetes</h1>
-		<h3 style="color:blue">Version: twogghub/k8s-workshop:1.0-baby</h3>	
+		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/1.1.png">
+		<h1 style="color:blue">Playing with Kubernetes</h1>
+    <h2 style="color:purple">Your server IP:` + getServerIP() + `</h2>
+		<h3 style="color:green">Version: twogghub/k8s-workshop:1.1-rolling</h3>	
 		</center></body></html>`
 	fmt.Fprintf(w, html)
 }
