1a96880e7a06fd3273dba0cfc7983dca6c2d94e1
Catherin Cruz <cruz.catherin@gmail.com>
2018-09-30 02:01:39 -0500

    Adding new header image

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
+var (
+	version  = "1.2-yaml"
+	bannedIp = "0.0.0.0"
+)
+
+func getServerIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP.String()
+}
+
 func playHome(w http.ResponseWriter, r *http.Request) {
 	html := `<!DOCTYPE html><html><body><center>
-		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/1.0.png">
+		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/1.2.png">
 		<h1 style="color:green">Playing with Kubernetes</h1>
-		<h3 style="color:blue">Version: twogghub/k8s-workshop:1.0-baby</h3>	
+		<h2 style="color:blue">Your server IP:` + getServerIP() + `</h2>
+		<h3 style="color:blue">Version: twogghub/k8s-workshop:` + version + `</h3>	
 		</center></body></html>`
 	fmt.Fprintf(w, html)
 }
