313fa66d3200678494a32deea33a1712b1f41784
Catherin Cruz <cruz.catherin@gmail.com>
2018-09-30 20:52:04 -0500

    Updating .go file to 1.3-liveness

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -6,6 +6,10 @@ import (
 	"net/http"
 )
 
+var (
+	bannedIp = "0.0.0.0"
+)
+
 func getServerIP() string {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
@@ -15,18 +19,54 @@ func getServerIP() string {
 	return conn.LocalAddr().(*net.UDPAddr).IP.String()
 }
 
+func getHTML(status string) string {
+	message := "DevOps, Mr. Cat speaking, how can I help you?"
+	image := "1.3.0"
+	color := "orange"
+	if status == "alive" {
+		message = "High five budy, I'm healthy as ever!"
+		image = "1.3.1"
+		color = "green"
+	}
+	if status == "dead" {
+		message = "I don't want to die Mr. Stark..."
+		image = "1.3.2"
+		color = "red"
+	}
+	return `<!DOCTYPE html><html><body><center>
+	<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/1.3-liveness/src/` + image + `.png">
+	<h1 style="color:` + color + `">` + message + `</h3>	
+	<h2 style="color:green">Playing with Kubernetes</h1>
+	<h2 style="color:blue">Server IP ` + getServerIP() + `</h2>
+	<h3 style="color:blue">Version twogghub/k8s-workshop:1.3-liveness</h3>	
+	</center></body></html>`
+}
+
 func playHome(w http.ResponseWriter, r *http.Request) {
-	html := `<!DOCTYPE html><html><body><center>
-		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/1.1.2.png">
-		<h1 style="color:orange">Playing with Kubernetes</h1>
-    		<h2 style="color:orange">Server IP ` + getServerIP() + `</h2>
-		<h3 style="color:orange">Version twogghub/k8s-workshop:1.1-rolling</h3>	
-		</center></body></html>`
-	fmt.Fprintf(w, html)
+	if bannedIp == getServerIP() {
+		fmt.Fprintf(w, getHTML("dead"))
+	} else {
+		fmt.Fprintf(w, getHTML("home"))
+	}
+}
+
+func playHealth(w http.ResponseWriter, r *http.Request) {
+	if getServerIP() == bannedIp {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	} else {
+		fmt.Fprintf(w, getHTML("alive"))
+	}
+}
+
+func playKillMe(w http.ResponseWriter, r *http.Request) {
+	bannedIp = getServerIP()
+	fmt.Fprintf(w, getHTML("dead"))
 }
 
 func main() {
 	http.HandleFunc("/", playHome)
+	http.HandleFunc("/health", playHealth)
+	http.HandleFunc("/killme", playKillMe)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
